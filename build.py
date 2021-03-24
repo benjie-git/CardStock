13881766ec78a1c7915f4388f433811a60d551c9
Ben Levitt <benjie@gmail.com>
2021-03-24 08:52:57 -0700

    add build script

---
+++ build.py
@@ -0,0 +1,27 @@
+import wx
+import os
+import shutil
+import PyInstaller.__main__
+
+"""PyInstaller commands for building the standalone Designer and Viewer apps on Mac and Windows"""
+
+def pyinstall(cmd):
+    args = cmd.split(" ")
+    PyInstaller.__main__.run(args)
+
+if wx.Platform == "__WXMAC__":
+    pyinstall("--onedir --clean -y --windowed -s -n standalone standalone.py")
+    os.mkdir("dist/containerDir")
+    shutil.move("dist/standalone.app", "dist/containerDir/")
+    pyinstall("--onedir --clean -y --windowed -s --exclude-module PyInstaller --add-data dist/containerDir:. -n CardStock_Designer designer.py")
+    pyinstall("--onedir --clean -y --windowed -s -n CardStock_Viewer viewer.py")
+
+elif wx.Platform == "__WXMSW__":
+    pyinstall("--onefile --clean -y --windowed -n standalone standalone.py")
+    pyinstall("--onefile --clean -y --windowed --exclude-module PyInstaller --add-data dist/standalone.exe;. -n CardStock_Designer designer.py")
+    pyinstall("--onefile --clean -y --windowed -n CardStock_Viewer viewer.py")
+
+else:
+    pyinstall("--onefile --clean -y --windowed -n standalone standalone.py")
+    pyinstall("--onefile --clean -y --windowed --exclude-module PyInstaller --add-data dist/standalone:. -n CardStock_Designer designer.py")
+    pyinstall("--onefile --clean -y --windowed -n CardStock_Viewer viewer.py")
