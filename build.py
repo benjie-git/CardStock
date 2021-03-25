e04f2bc6c93c61ecd10d77e41bd2bdbd00d202f6
Ben Levitt <benjie@gmail.com>
2021-03-25 11:55:29 -0700

    improve mac build process

--- build.py
+++ build.py
@@ -10,11 +10,14 @@ def pyinstall(cmd):
     PyInstaller.__main__.run(args)
 
 if wx.Platform == "__WXMAC__":
-    pyinstall("--onedir --clean -y --windowed -s -n standalone standalone.py")
+    pyinstall("--onedir --clean -y --windowed -n standalone standalone.py")
+    shutil.rmtree("dist/containerDir")
+    # using --add-data to add a directory includes its contents, not the directory itself, and since apps are
+    # directories, then in order to add the app, we need to --add-data a container directory with the app inside of it.
     os.mkdir("dist/containerDir")
     shutil.move("dist/standalone.app", "dist/containerDir/")
-    pyinstall("--onedir --clean -y --windowed -s --exclude-module PyInstaller --add-data dist/containerDir:. -n CardStock_Designer designer.py")
-    pyinstall("--onedir --clean -y --windowed -s -n CardStock_Viewer viewer.py")
+    pyinstall("--onedir --clean -y --windowed --exclude-module PyInstaller --add-data dist/containerDir:. -n CardStock_Designer designer.py")
+    pyinstall("--onedir --clean -y --windowed -n CardStock_Viewer viewer.py")
 
 elif wx.Platform == "__WXMSW__":
     pyinstall("--onefile --clean -y --windowed -n standalone standalone.py")
