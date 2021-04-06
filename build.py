bc86278dbe0a17fe543e6ab324f07b323c1197ec
Ben Levitt <benjie@gmail.com>
2021-04-06 09:05:43 -0700

    Improve build script

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ import wx
 import os
 import shutil
 import PyInstaller.__main__
+import version
 
 """PyInstaller commands for building the standalone Designer and Viewer apps on Mac and Windows"""
 
@@ -10,22 +11,89 @@ def pyinstall(cmd):
     PyInstaller.__main__.run(args)
 
 if wx.Platform == "__WXMAC__":
+    # Build the standalone binary
     pyinstall("--onedir --clean -y --windowed -n standalone standalone.py")
-    if os.path.exists("dist/containerDir"):
-        shutil.rmtree("dist/containerDir")
+    shutil.rmtree("dist/standalone")
+
+    # Build the designer binary
     # using --add-data to add a directory includes its contents, not the directory itself, and since apps are
     # directories, then in order to add the app, we need to --add-data a container directory with the app inside of it.
+    if os.path.exists("dist/containerDir"):
+        shutil.rmtree("dist/containerDir")
     os.mkdir("dist/containerDir")
     shutil.move("dist/standalone.app", "dist/containerDir/")
     pyinstall("--onedir --clean -y --windowed --exclude-module PyInstaller --add-data dist/containerDir:. -n CardStock_Designer designer.py")
+    shutil.rmtree("dist/containerDir")
+    shutil.rmtree("dist/CardStock_Designer")
+
+    # Build the viewer binary
     pyinstall("--onedir --clean -y --windowed -n CardStock_Viewer viewer.py")
+    shutil.rmtree("dist/CardStock_Viewer")
+
+    # Build the package directory
+    package_dir = f"dist/CardStock_v{version.VERSION}"
+    if os.path.exists(package_dir):
+        shutil.rmtree(package_dir)
+    os.mkdir(package_dir)
+    shutil.move("dist/CardStock_Designer.app", package_dir)
+    shutil.move("dist/CardStock_Viewer.app", package_dir)
+    shutil.copytree("examples", package_dir + "/examples")
+    shutil.copy("README.md", package_dir)
+    shutil.copy("LICENSE", package_dir)
+
+    # More cleanup
+    shutil.rmtree("__pycache__")
+    shutil.rmtree("build")
+    os.remove("standalone.spec")
+    os.remove("CardStock_Designer.spec")
+    os.remove("CardStock_Viewer.spec")
+
 
 elif wx.Platform == "__WXMSW__":
+    # Build the binaries
     pyinstall("--onefile --clean -y --windowed -n standalone standalone.py")
     pyinstall("--onefile --clean -y --windowed --exclude-module PyInstaller --add-data dist/standalone.exe;. -n CardStock_Designer designer.py")
     pyinstall("--onefile --clean -y --windowed -n CardStock_Viewer viewer.py")
 
+    # Build the package directory
+    package_dir = f"dist/CardStock_v{version.VERSION}"
+    if os.path.exists(package_dir):
+        shutil.rmtree(package_dir)
+    os.mkdir(package_dir)
+    shutil.move("dist/CardStock_Designer.exe", package_dir)
+    shutil.move("dist/CardStock_Viewer.exe", package_dir)
+    shutil.copytree("examples", package_dir + "/examples")
+    shutil.copy("README.md", package_dir)
+    shutil.copy("LICENSE", package_dir)
+
+    # More cleanup
+    shutil.rmtree("__pycache__")
+    shutil.rmtree("build")
+    os.remove("standalone.spec")
+    os.remove("CardStock_Designer.spec")
+    os.remove("CardStock_Viewer.spec")
+
+
 else:
+    # Build the binaries
     pyinstall("--onefile --clean -y --windowed -n standalone standalone.py")
     pyinstall("--onefile --clean -y --windowed --exclude-module PyInstaller --add-data dist/standalone:. -n CardStock_Designer designer.py")
     pyinstall("--onefile --clean -y --windowed -n CardStock_Viewer viewer.py")
+
+    # Build the package directory
+    package_dir = f"dist/CardStock_v{version.VERSION}"
+    if os.path.exists(package_dir):
+        shutil.rmtree(package_dir)
+    os.mkdir(package_dir)
+    shutil.move("dist/CardStock_Designer", package_dir)
+    shutil.move("dist/CardStock_Viewer", package_dir)
+    shutil.copytree("examples", package_dir + "/examples")
+    shutil.copy("README.md", package_dir)
+    shutil.copy("LICENSE", package_dir)
+
+    # More cleanup
+    shutil.rmtree("__pycache__")
+    shutil.rmtree("build")
+    os.remove("standalone.spec")
+    os.remove("CardStock_Designer.spec")
+    os.remove("CardStock_Viewer.spec")
