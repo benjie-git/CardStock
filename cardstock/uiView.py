77ea16c1cb3c45e6955b953108546b735fcad639
Ben Levitt <benjie@gmail.com>
2021-12-13 11:51:49 -0800

    Improve object reordering speed, for non-native objects

--- cardstock/uiView.py
+++ cardstock/uiView.py
@@ -747,7 +747,13 @@ class ViewModel(object):
         self.parent.childModels.remove(self)
         self.parent.childModels.insert(index, self)
         if self.GetCard() == self.stackManager.uiCard.model:
-            self.stackManager.LoadCardAtIndex(self.stackManager.cardIndex, reload=True)
+            ui = self.stackManager.GetUiViewByModel(self)
+            if ui.view:
+                self.stackManager.LoadCardAtIndex(self.stackManager.cardIndex, reload=True)
+            else:
+                ui.parent.uiViews.remove(ui)
+                ui.parent.uiViews.insert(index, ui)
+                self.stackManager.view.Refresh()
 
     def OrderMoveBy(self, delta):
         index = self.parent.childModels.index(self) + delta
@@ -1204,7 +1210,7 @@ class ViewProxy(object):
         if i < 0 or i >= len(model.parent.childModels):
             raise TypeError("index is out of bounds")
 
-        @RunOnMainAsync
+        @RunOnMainSync
         def f():
             if model.didSetDown: return
             model.OrderMoveTo(i)
