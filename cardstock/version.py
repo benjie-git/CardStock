6be9535657e88c86979950ef75b09af51084128a
Ben Levitt <benjie@gmail.com>
2023-10-16 10:53:46 -0700

    version bump to 0.99.4

--- cardstock/version.py
+++ cardstock/version.py
@@ -6,5 +6,5 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.  If a copy
 # of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-VERSION = '0.99.3'
+VERSION = '0.99.4'
 FILE_FORMAT_VERSION = 7
