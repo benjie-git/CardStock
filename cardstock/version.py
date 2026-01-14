bba03076d671c2d46fe45f8b42df604b5c56d413
Ben Levitt <benjie@gmail.com>
2026-01-14 14:09:22 -0800

    Version bump to 0.99.9, and build system fixes

--- cardstock/version.py
+++ cardstock/version.py
@@ -6,5 +6,5 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.  If a copy
 # of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-VERSION = '0.99.8'
+VERSION = '0.99.9'
 FILE_FORMAT_VERSION = 10
