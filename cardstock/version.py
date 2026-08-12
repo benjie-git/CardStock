aa8f38b199634eeefab8b5cc09e04b64b36aafc6
Ben Levitt <benjie@gmail.com>
2026-08-11 23:26:37 -0700

    Version and copyright year bumps

--- cardstock/version.py
+++ cardstock/version.py
@@ -1,10 +1,10 @@
 # This file is part of CardStock.
 #     https://github.com/benjie-git/CardStock
 #
-# Copyright Ben Levitt 2020-2023
+# Copyright Ben Levitt 2020-2026
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.  If a copy
 # of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 FILE_FORMAT_VERSION = 10
